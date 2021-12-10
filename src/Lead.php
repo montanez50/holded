b8893acc4934a1a8c6aa3d639fff878ee6797370
vshopes <angel@vshop.es>
2021-12-10 23:40:48 +0100

    CrmApi

---
+++ src/Lead.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Holded;
+use Holded\Abstracts\InvoiceApi;
+
+/**
+ * Class Contacts
+ * @package Holded
+ *
+ */
+class Leads extends CrmApi
+{
+
+    protected $methods = ['list','create','get','update','delete' ];
+
+    protected $pluralizeMethods = [];
+
+    protected $endpoint = 'leads';
+
+    public function __construct(Caller $call)
+    {
+        $this->call = $call;
+    }
+}
